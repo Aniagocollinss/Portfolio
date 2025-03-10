0254a202ebaff7cc988f637d8292d0dc2c0d2df4
Aniagocollinss <Chiwenducollinss@gmail.com>
2025-03-10 09:08:27 +0100

    Delete About_me directory

--- About_me/aboutmefade.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector(".content").style.opacity = "1";
-    document.querySelector(".content").style.transform = "translateY(0)";
-});
